fbc69bccaee821758dc5cc23f500eda2d60a1db5
Anh Tuan Tran <baotudauanhtuan@gmail.com>
2021-08-21 21:06:30 +0700

    21/8

--- js/main.js
+++ js/main.js
@@ -7,4 +7,13 @@ $(document).ready(function () {
     $toggleCollapse.click(function () {
         $nav.toggleClass('collapse');
     });
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        
+    });
 });
