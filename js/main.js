a206c548e2251e39adb29ce0e93ac937f002fe6c
Anh Tuan Tran <baotudauanhtuan@gmail.com>
2021-08-22 21:29:50 +0700

    blogger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
 $(document).ready(function () {
 
     $nav = $('.nav');
@@ -14,6 +28,16 @@ $(document).ready(function () {
         dots: false,
         nav: true,
         navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-        
+        responsive: responsive
     });
+
+    //click to scroll top
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+    
+    //AOS instance
+    AOS.init();
 });
